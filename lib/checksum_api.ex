4a1e1977ebb03adea32d0b6126a2ea3827fdf2f2
okothkongo <okothkongo@gmail.com>
2021-03-25 10:47:28 +0300

    initial commit

---
+++ lib/checksum_api.ex
@@ -0,0 +1,18 @@
+defmodule ChecksumApi do
+  @moduledoc """
+  Documentation for `ChecksumApi`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ChecksumApi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/checksum_api_test.exs
@@ -0,0 +1,8 @@
+defmodule ChecksumApiTest do
+  use ExUnit.Case
+  doctest ChecksumApi
+
+  test "greets the world" do
+    assert ChecksumApi.hello() == :world
+  end
+end
