cc2e039b1de96a11abeb1d79e912d20df53ddca3
okoth kongo <okothkongo@gmail.com>
2021-12-17 18:10:15 +0300

    move checksum/1 func to the Core module

--- lib/checksum_api.ex
+++ lib/checksum_api.ex
@@ -2,41 +2,21 @@ defmodule ChecksumApi do
   @moduledoc """
   Documentation for `ChecksumApi`.
   """
+  alias ChecksumApi.Core
 
+  @spec add_digits(list(integer())) :: :ok
   def add_digits(digits) do
     GenServer.cast(:checksum_api, {:add_digits, digits})
   end
 
+  @spec clear_digits :: :ok
   def clear_digits() do
     GenServer.cast(:checksum_api, :clear_digits)
   end
 
   @spec checksum() :: integer
   def checksum() do
-    read_current_sate()
-    |> sum_of_odd_and_even()
-    |> total_sum()
-    |> rem(10)
-    |> case do
-      0 -> 0
-      rem -> 10 - rem
-    end
-  end
-
-  defp total_sum({odd, even}) do
-    odd + even * 3
-  end
-
-  defp sum_of_odd_and_even(digits) do
-    digits
-    |> Enum.with_index()
-    |> Enum.reduce({0, 0}, fn {value, index}, {odd, even} ->
-      if rem(index, 2) == 0 do
-        {odd, even + value}
-      else
-        {odd + value, even}
-      end
-    end)
+    Core.checksum(read_current_sate())
   end
 
   defp read_current_sate() do
--- test/checksum_api_test.exs
+++ test/checksum_api_test.exs
@@ -1,8 +1,3 @@
 defmodule ChecksumApiTest do
   use ExUnit.Case
-
-  test "checksum/1 returns  checksum integer" do
-    ChecksumApi.add_digits(5_489_850_354)
-    assert ChecksumApi.checksum() == 7
-  end
 end
